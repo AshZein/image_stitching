b1562772be3b36f0bf672599b054ca585f302e9f
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-01-26 20:06:32 -0500

    ratio test and sorting matches

--- feature_mapping.py
+++ feature_mapping.py
@@ -8,13 +8,18 @@ def hamming_distance(descripor1, descripor2):
 def bruteforce_matcher(descriptors1, descriptors2):
     matches = []
     for i in range(len(descriptors1)):
-        min_distance = float('inf')
-        min_index = -1
+        distances = []
         for j in range(len(descriptors2)):
             distance = hamming_distance(descriptors1[i], descriptors2[j])
-            if distance < min_distance:
-                min_distance = distance
-                min_index = j
-        matches.append(cv2.DMatch(i, min_index, min_distance))
-        
+            distances.append((distance, j))
+        distances.sort(key=lambda x: x[0])
+        matches.append((cv2.DMatch(i, distances[0][1], distances[0][0]), cv2.DMatch(i, distances[1][1], distances[1][0])))
     return matches
+
+
+def ratio_test(matches, ratio=0.75):
+    good_matches = []
+    for m, n in matches:
+        if m.distance < ratio * n.distance:
+            good_matches.append(m)
+    return good_matches
